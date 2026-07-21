e96e8e0ec3706210fa271982739d66554346e990
amineb3500-cmyk <amineb3500@gmail.com>
2026-07-21 14:52:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from telegram import Update from telegram.ext import Application, CommandHandler, ContextTypes import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text("🤖 ربات امین روشن است")
 async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text("🏓 Pong!")
 app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler("start", start)) app.add_handler(CommandHandler("ping", ping))
-app.run_polling()
+app.add_handler(CommandHandler("ping", ping))
+app.run_polling() 
