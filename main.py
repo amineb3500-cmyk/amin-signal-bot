cc3cc233fd22d61f25526ebe72c5f56197307acc
amineb3500-cmyk <amineb3500@gmail.com>
2026-07-21 14:39:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-import os
-import yfinance as yf
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text('🤖 ربات سیگنال امین فعال شد!')
-async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
-await update.message.reply_text('🏓 Pong! ربات آنلاین است.')
-async def price(update: Update, context: ContextTypes.DEFAULT_TYPE):
-btc = yf.Ticker('BTC-USD')
-data = btc.history(period='1d')
-current_price = data['Close'].iloc[-1]
-await update.message.reply_text(
-f'₿ قیمت فعلی بیت‌کوین:\n{current_price:.2f} دلار'
-)
+from telegram import Update from telegram.ext import Application, CommandHandler, ContextTypes import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text("🤖 ربات امین روشن است")
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE): await update.message.reply_text("🏓 Pong!")
 app = Application.builder().token(BOT_TOKEN).build()
-app.add_handler(CommandHandler('start', start))
-app.add_handler(CommandHandler('ping', ping))
-app.add_handler(CommandHandler('price', price))
+app.add_handler(CommandHandler("start", start)) app.add_handler(CommandHandler("ping", ping))
 app.run_polling()
