3a59dfb9219cf0ba5883d4c0196891c45c3c37aa
amineb3500-cmyk <amineb3500@gmail.com>
2026-07-21 15:06:23 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def ping(update: Update, context: 
+ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text("🏓 Pong!")
 app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("ping", ping))
